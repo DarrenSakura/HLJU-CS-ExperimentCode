b5496a9fc50bfded5fab8b045a7f84024e9b67ff
DarrenSakura <Darrensakura@gmail.com>
2026-03-17 14:16:13 +0800

    commit

---
+++ 虚拟与增强现实技术/Math/Assets/Math.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+public class Math : MonoBehaviour
+{
+    public Transform pointPrefab;
+    public int resolution = 100;
+    Transform[] points;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 120;
+        float step = 2f / resolution;
+        Vector3 scale = Vector3.one * step;
+        Vector3 position= Vector3.zero;
+        points=new Transform[resolution];
+        for (int i = 0; i < resolution; i++)
+        {
+            
+            Transform point = Instantiate(pointPrefab);
+            position.x = (i + 0.5f) * step - 1f;
+            point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(transform, false);
+            points[i] = point;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x+Time.time));
+            point.localPosition = position;
+        }
+    }
+}
